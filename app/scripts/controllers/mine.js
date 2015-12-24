a0069a4e7130fa499cd536711b61804634e88055
unknown <zhiyuans@NKGWZHIYUANS00.citrite.net>
2015-12-24 19:58:11 +0800

    测试angularjs promise

--- app/scripts/controllers/mine.js
+++ app/scripts/controllers/mine.js
@@ -5,6 +5,31 @@
  * Created by ZhiyuanSun on 15/12/1.
  */
 ctrlModule
-    .controller('mineCtrl',['$scope',function($scope){
-        console.log('mineCtrl');
+    .controller('mineCtrl',['$scope', '$timeout', '$q',function($scope, $timeout, $q){
+        <!-- lang: js -->
+        var deferred = $q.defer();
+        var promise = deferred.promise;
+
+        // resolve it after a second
+        $timeout(function() {
+            deferred.resolve('foo');
+        }, 1000);
+
+        promise
+            .then(function(one) {
+                console.log('Promise one resolved with ', one);
+
+                var anotherDeferred = $q.defer();
+
+                // resolve after another second
+
+                $timeout(function() {
+                    anotherDeferred.resolve('bar');
+                }, 3000);
+
+                return anotherDeferred.promise;
+            })
+            .then(function(two) {
+                console.log('Promise two resolved with ', two);
+            });
     }]);
