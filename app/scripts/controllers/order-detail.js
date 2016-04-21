8c0a57b32609bb5775eb400b99f1bc8cae651ae0
Zhiyuan Sun <sunzy0212@163.com>
2016-04-21 22:35:09 +0800

    修改order-detail样式

--- app/scripts/controllers/order-detail.js
+++ app/scripts/controllers/order-detail.js
@@ -10,7 +10,8 @@ ctrlModule
 		var orderId = $stateParams.orderId;
     userOrders.getUserOrderDetailByOrderId(orderId)
       .then(function(data){
-        $scope.orderDetail = data;
+        console.log(data);
+        $scope.orderDetail = dataProxy(data);
       });
 
 
@@ -20,6 +21,25 @@ ctrlModule
       var scrollHeight = window.innerHeight - headerHeight - footerHeight;
       scrollHeight += 'px';
       $("ion-scroll").css("height",scrollHeight);
+    }
+
+    function dataProxy(data){
+      var statusMap = {
+        "1": "未付款",
+        "2": "待评价",
+        "3": "已完成"
+      };
+
+      data.status = statusMap[data.status];
+      data.time = formatTime(new Date(data.time));
+      return data;
+    }
+
+    // format a Date object to "YYYY-MM-DD hh:mm:ss"
+    function formatTime(obj){
+      var dateArr = [obj.getFullYear(), obj.getMonth()+1, obj.getDate()];
+      var timeArr = [obj.getHours(), obj.getMinutes(), obj.getSeconds()];
+      return dateArr.join('-') + ' ' + timeArr.join(':');
     }
 	}
 ]);
